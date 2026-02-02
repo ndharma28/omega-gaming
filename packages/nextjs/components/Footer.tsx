093c7c1e5a69e59319ace00ffd9ee4e40912b355
ndharma28 <ndharmavaram@noctrl.edu>
2026-02-02 13:57:08 -0600

    footer fixed

--- packages/nextjs/components/Footer.tsx
+++ packages/nextjs/components/Footer.tsx
@@ -3,11 +3,11 @@ import Link from "next/link";
 import { hardhat } from "viem/chains";
 import { MagnifyingGlassIcon } from "@heroicons/react/24/outline";
 import { HeartIcon } from "@heroicons/react/24/outline";
+import { EtherConverter } from "~~/components/EtherConverter";
 import { SwitchTheme } from "~~/components/SwitchTheme";
 import { BuidlGuidlLogo } from "~~/components/assets/BuidlGuidlLogo";
 import { Faucet } from "~~/components/scaffold-eth";
 import { useTargetNetwork } from "~~/hooks/scaffold-eth/useTargetNetwork";
-import { EtherConverter } from "~~/components/EtherConverter";
 
 /**
  * Site footer
@@ -21,9 +21,7 @@ export const Footer = () => {
       <div>
         <div className="fixed flex justify-between items-center w-full z-10 p-4 bottom-0 left-0 pointer-events-none">
           <div className="flex flex-col md:flex-row gap-2 pointer-events-auto">
-            {/* REPLACED: The old price display code is gone.
-                ADDED: The EtherConverter component is here now. 
-            */}
+            {/* ✅ FIXED: The Price button code is gone. Only EtherConverter is here. */}
             <EtherConverter />
 
             {isLocalNetwork && (
