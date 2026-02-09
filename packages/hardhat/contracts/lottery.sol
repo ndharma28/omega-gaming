af2060979c9c869433c9bc0595e30efd14ae5195
ndharma28 <ndharmavaram@noctrl.edu>
2026-02-09 14:32:03 -0600

    changed name to lottery

--- packages/hardhat/contracts/lottery.sol
+++ packages/hardhat/contracts/lottery.sol
@@ -5,7 +5,7 @@ import {ReentrancyGuard} from "@openzeppelin/contracts/utils/ReentrancyGuard.sol
 import {VRFConsumerBaseV2Plus} from "@chainlink/contracts/src/v0.8/vrf/dev/VRFConsumerBaseV2Plus.sol";
 import {VRFV2PlusClient} from "@chainlink/contracts/src/v0.8/vrf/dev/libraries/VRFV2PlusClient.sol";
 
-contract lotteryV3 is
+contract Lottery is
     VRFConsumerBaseV2Plus,
     ReentrancyGuard
 {
