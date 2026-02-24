2d9114579ba497b626ee85c7dae9ce96ae3551e4
ndharma28 <ndharmavaram@noctrl.edu>
2026-02-24 16:10:55 -0600

    integrates new smart contract and removes random number smart contract

--- packages/hardhat/deploy/00_deploy_lottery.ts
+++ packages/hardhat/deploy/00_deploy_lottery.ts
@@ -3,22 +3,16 @@ import { DeployFunction } from "hardhat-deploy/types";
 
 const deployLottery: DeployFunction = async function (hre: HardhatRuntimeEnvironment) {
   const { deployer } = await hre.getNamedAccounts();
-  const { deployments } = hre;
-  const { deploy } = deployments;
+  const { deploy } = hre.deployments;
+
+  // Sepolia testnet values
+  const VRF_COORDINATOR = "0x9DdfaCa8183c41ad55329BdeeD9F6A8d53168B1B";
+  const SUBSCRIPTION_ID = "5381939440800401583750118558724030775370857736705249184581988840504175043599";
+  const KEY_HASH = "0x787d74caea10b2b357790d5b5247c2f63d1d91572a9846f780606e4d953677ae";
 
   await deploy("OmegaLottery", {
     from: deployer,
-    contract: "OmegaLottery",
-    // UUPS Upgradeable contracts use a Proxy
-    proxy: {
-      proxyContract: "OpenZeppelinTransparentProxy", // hardhat-deploy handles UUPS through this
-      execute: {
-        init: {
-          methodName: "initialize",
-          args: [deployer], // This passes your address to the 'initialOwner' arg
-        },
-      },
-    },
+    args: [SUBSCRIPTION_ID, VRF_COORDINATOR, KEY_HASH],
     log: true,
     autoMine: true,
   });
